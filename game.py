05850dfce95e97c70c12dffb557852c3a2770ee7
Obsoleete <47087766+Obsoleete@users.noreply.github.com>
2019-11-10 15:45:45 -0500

    Implements the end_game_display method for game.py
    Implements the end_game_display method for game.py. This method 
    is crucial as it allows the end screen to be displayed after a player 
    dies. It allows the player to restart the game or quit it.

--- game.py
+++ game.py
@@ -53,8 +53,53 @@ class Game:
         This method creates and displays a pygame window with the appropriate
         message displayed
         """
-        # To be implemented
-        pass
+        pygame.init()
+        end_screen = True
+
+        while end_screen:
+            red_font = (255, 0, 0)
+            white_font = (255, 255, 255)
+            screen = pygame.display
+            surface = screen.set_mode((800, 600))
+            pygame.display.set_caption('Sorry! You Lost.')
+
+            # Creates text for end screen, when snake dies
+            large_text = pygame.font.Font('freesansbold.ttf', 90)
+            text_surf = large_text.render('Sorry! You Died.', True, red_font)
+            text_rect = text_surf.get_rect()
+            text_rect.center = (400, 300)
+            surface.blit(text_surf, text_rect)
+
+            # Creates two rectangles, which will act as buttons
+            pygame.draw.rect(surface, red_font, (150, 450, 100, 50))
+            pygame.draw.rect(surface, red_font, (550, 450, 100, 50))
+
+            # Creates the text for the restart button
+            small_text = pygame.font.Font('freesansbold.ttf', 25)
+            button_restart_text = small_text.render('Restart', True, white_font)
+            button_restart_rect = text_surf.get_rect()
+            button_restart_rect.center = (504, 510)
+            surface.blit(button_restart_text, button_restart_rect)
+
+            # Creates text for te quit button
+            button_quit_text = small_text.render('Quit', True, white_font)
+            button_quit_rect = text_surf.get_rect()
+            button_quit_rect.center = (920, 510)
+            surface.blit(button_quit_text, button_quit_rect)
+            screen.flip()
+
+            mouse_pos = pygame.mouse.get_pos()
+
+            # If the mouse positions are in the area of the restart or quit buttons, carry out the appropriate action
+            if 250 > mouse_pos[0] > 150 and 500 > mouse_pos[1] > 450:
+                pygame.quit()
+                end_screen = False
+                new_game = Game()
+                new_game.run_game()
+            if 550 + 100 > mouse_pos[0] > 550 and 450 + 50 > mouse_pos[1] > 450:
+                end_screen = False
+                pygame.quit()
+
 
 
 def get_score(game_object):
